20ba2b211c966e4660fcbd43540241edbdc1b3fc
Krista Galang <krista.galang98@gmail.com>
2021-08-26 14:21:23 -0400

    Section 2.4: Create HTML Markup for Landing and Define Common Styles

---
+++ foods/script.js
@@ -0,0 +1,20 @@
+const icons = document.querySelectorAll('.section-1 .section-1-icons i')
+let i = 1
+debugger;
+setInterval(() => {
+    i++
+
+    
+
+    const icon = document.querySelector('.section-1-icons .change');
+
+    icon.classList.remove('change');
+
+    if(i > icons.length) {
+        icons[0].classList.add('change');
+        i = 1;
+    } else {
+        icon.nextElementSibling.classList.add('change');
+    }
+   
+}, 4000)
